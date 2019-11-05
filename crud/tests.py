8dd63de6d57768433197d10c98d4e300b9e56bc7
Anderson-Bernardy <andersonbmartinelli@gmail.com>
2019-11-05 07:07:08 -0300

    first test

--- crud/tests.py
+++ crud/tests.py
@@ -10,15 +10,12 @@ class MyAcademus(SimpleTestCase):
     def test_student_form(self):
         self.driver = webdriver.Firefox()
         self.driver.get("http://localhost:8000/student/new")
-        self.driver.implicitly_wait(10)
-        # self.driver.find_element_by_name("newaluno").click()
-        # self.driver.find_element_by_name("name").send_keys("Pedro dos Santos")
-        # self.driver.find_element_by_name("birthdate").send_keys("20/11/1996")
-        # self.driver.find_element_by_name("cpf").send_keys("02288312376")
-        # self.driver.find_element_by_name("course").send_keys("Matemática")
-        # self.driver.find_element_by_name("save").click()
+        self.driver.find_element_by_name("name").send_keys("Pedro dos Santos")
+        self.driver.find_element_by_name("birthdate").send_keys("20-11-1996")
+        self.driver.find_element_by_name("cpf").send_keys("02288312376")
+        self.driver.find_element_by_name("course").send_keys("Matemática")
+        self.driver.find_element_by_name("save").click()
         self.driver.close()
-        self.driver.quit()
         
 if __name__ == '__main__':
     academus = MyAcademus()
