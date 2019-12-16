fea033bfce527bd110f61dbf3a3c7d277440e7ce
Ellen Fernandes <ellie.frnds@gmail.com>
2019-12-16 16:10:11 -0300

    testes

--- my_academus/crud/tests.py
+++ my_academus/crud/tests.py
@@ -1,38 +1,58 @@
-from django.test import TestCase
-from django.test import SimpleTestCase
 from selenium import webdriver
-import unittest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.urls import reverse
+import time
 
 
 # Create your tests here.
-class TestMyAcademus(SimpleTestCase):
 
-    @unittest.expectedFailure
+class MyAcademusTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome("C:\Ellen\chromedriver.exe")
+
+    def tearDown(self):
+        self.browser.close()
+
     def test_student_form(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get("http://localhost:8000/student/new")
-        self.driver.find_element_by_name("name").send_keys("Pedro dos Santos")
-        self.driver.find_element_by_name("birthdate").send_keys("20-11-1996")
-        self.driver.find_element_by_name("cpf").send_keys("02288312376")
-        self.driver.find_element_by_name("course").send_keys("Matemática")
-        self.driver.find_element_by_name("save").click()
-        self.driver.close()
-        
+        self.browser.get("http://localhost:8000/student/new")
+        self.browser.find_element_by_name("name").send_keys("Pedro dos Santos")
+        self.browser.find_element_by_name("birthdate").send_keys("20/11/1996")
+        self.browser.find_element_by_name("cpf").send_keys("02288312376")
+        self.browser.find_element_by_name("course").send_keys("Matemática")
+        time.sleep(20)
+        # self.browser.find_element_by_name("save").click()
+    
+    # def test_course_form(self):
+    #     self.browser.get("http://localhost:8000/course/new")
+    #     self.browser.find_element_by_name("name").send_keys("Administração")
+    #     self.browser.find_element_by_name("initials").send_keys("ADM")
+    #     self.browser.find_element_by_name("description").send_keys("Descrição do curso aqui")
+    #     self.browser.find_element_by_name("duration").send_keys("Quatro Anos")
+    #     # self.browser.find_element_by_name("save").click()
+    
+    # def test_discipline_form(self):
+    #     self.browser.get("http://localhost:8000/discipline/new")
+    #     self.browser.find_element_by_name("name").send_keys("Engenharia de Software 1")
+    #     self.browser.find_element_by_name("initials").send_keys("ES1")
+    #     self.browser.find_element_by_name("description").send_keys("Descrição da matéria aqui")
+    #     self.browser.find_element_by_name("course").send_keys("Ciência da Computação")
+    #     self.browser.find_element_by_name("academic_period").send_keys("Terceiro Ano")
+    #     # self.browser.find_element_by_name("save").click()
 
-
-# Example Test Cases
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    # def test_class_form(self):
+    #     self.browser.get("http://localhost:8000/class/new")
+    #     self.browser.find_element_by_name("code").send_keys("ES1_2019A")
+    #     self.browser.find_element_by_name("discipline").send_keys("CC - Engenharia de Software 1")
+    #     self.browser.find_element_by_name("start_date").send_keys("10/02/2019")
+    #     self.browser.find_element_by_name("end_date").send_keys("20/11/2019")
+    #     self.browser.find_element_by_name("assessment[]").send_keys("Prova 1")
+    #     self.browser.find_element_by_name("add_assessment").click()
+    #     self.browser.find_element_by_name("assessment[]").send_keys("Trabalho 1")
+    #     self.browser.find_element_by_name("class_week[]").send_keys("Terça")
+    #     self.browser.find_element_by_name("class_time[]").send_keys("17:00")
+    #     self.browser.find_element_by_name("add_class_time").click()
+    #     self.browser.find_element_by_name("class_week[]").send_keys("Quinta")
+    #     self.browser.find_element_by_name("class_time[]").send_keys("10:30")
+    #     #self.browser.find_element_by_name("save").click()
+    #     time.sleep(20)
--- my_academus/my_academus/settings.py
+++ my_academus/my_academus/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'my_academus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#      }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_academus',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST': 'mariadb-academus',
-        'PORT': '3306',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'my_academus',
+#         'USER': 'root',
+#         'PASSWORD': 'admin',
+#         'HOST': 'mariadb-academus',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
